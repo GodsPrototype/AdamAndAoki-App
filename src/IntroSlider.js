165b0706bb4ff440d5546a238847071ec9a916d9
unknown <kevinwaller@ziggo.nl>
2018-06-24 03:07:58 +0200

    Working on introslider

--- src/IntroSlider.js
+++ src/IntroSlider.js
@@ -8,5 +8,17 @@ const slides = [
     title: 'UV & Weather',
     text: 'Too much UV is bad\n\nCheck the UV Index and Weather at your current location',
     image:
+  },
+  {
+    key: 'screen1',
+    title: 'UV & Weather',
+    text: 'Too much UV is bad\n\nCheck the UV Index and Weather at your current location',
+    image:
+  },
+  {
+    key: 'screen1',
+    title: 'UV & Weather',
+    text: 'Too much UV is bad\n\nCheck the UV Index and Weather at your current location',
+    image:
   }
 ]
