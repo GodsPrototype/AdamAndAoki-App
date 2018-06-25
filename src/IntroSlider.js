06b402cf48a7ae02ebdd4a2e8446ba3cfee2dede
unknown <kevinwaller@ziggo.nl>
2018-06-25 21:31:19 +0200

    Changed introslider logos to be white, exported the final android apk under name 'adamandaoki-android-unsigned.apk' in directory '~/android/app/buid/outputs/apk'

--- src/IntroSlider.js
+++ src/IntroSlider.js
@@ -11,21 +11,21 @@ const slides = [
     key: 'screen1',
     title: 'UV Index',
     text: 'Too much UV is bad\n\nCheck the UV Index at your location regularly\nto limit daily UV exposure',
-    image: require('./images/baseline_wb_sunny_black_48dp.png'),
+    image: require('./images/baseline_wb_sunny_white_48dp.png'),
     backgroundColor: '#3300a6',
   },
   {
     key: 'screen2',
     title: 'Check the Weather',
     text: 'Check the local weather forecast and weather radar',
-    image: require('./images/baseline_wb_cloudy_black_48dp.png'),
+    image: require('./images/baseline_wb_cloudy_white_48dp.png'),
     backgroundColor: '#3300a6',
   },
   {
     key: 'screen3',
     title: 'Warn your Family',
     text: "Track and notify family members and their UV exposure\nYou can send personal recommendations based on skin type\nand the current UV Index",
-    image: require('./images/baseline_people_black_48dp.png'),
+    image: require('./images/baseline_people_white_48dp.png'),
     backgroundColor: '#3300a6',
   }
 ];
